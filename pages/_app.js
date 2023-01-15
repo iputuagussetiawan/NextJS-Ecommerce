084dfc17bf4d2a48e5e48b42f440f76eec7ecee6
iputuagussetiawan <agussetiawaniputu@gmail.com>
2023-01-15 20:16:46 +0800

    20. JSON web tokens & Mongodb adapter

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux'
 import store from "../store";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
+import Head from 'next/head';
 
 import { SessionProvider } from "next-auth/react"
 
@@ -11,6 +12,14 @@ let persistor = persistStore(store);
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <>
+    <Head>
+      <title>Green Line Shop</title>
+      <meta
+        name="description"
+        content="Green Line Shopping Service for all of your need"
+      ></meta>
+      <link rel="icon" href="/favison.ico"></link>
+    </Head>
       <SessionProvider session={session}>
         <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
