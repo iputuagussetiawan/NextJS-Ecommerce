096aadbed308f6787e6a3cde775319ae88a930b3
iputuagussetiawan <agussetiawaniputu@gmail.com>
2023-01-15 19:42:25 +0800

    19. Next auth setup

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home({country}) {
 	const { data: session } = useSession()
+	
 	return (
 		<div>
 			<Header country={country}/>
