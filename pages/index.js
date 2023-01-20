d66d96b0ec26174dfdc946f064ec79ce1a82d6b6
iputuagussetiawan <agussetiawaniputu@gmail.com>
2023-01-20 22:27:23 +0800

    27. Sign in page 2

--- pages/index.js
+++ pages/index.js
@@ -12,8 +12,6 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home({country}) {
 	const { data: session } = useSession()
-	console.log(session)
-	
 	return (
 		<div>
 			<Header country={country}/>
--- pages/signin.js
+++ pages/signin.js
@@ -1,7 +1,32 @@
-import React from 'react'
+import Header from '../components/header'
+import Footer from '../components/footer'
+import styles from '../styles/signin.module.scss'
+import {BiLeftArrowAlt} from "react-icons/bi"
+import Link from 'next/dist/client/link'
 
-export default function signin() {
+export default function signin({country}) {
   return (
-    <div>signin</div>
+    <>
+      <Header country={country}></Header>
+      <div className={styles.login}>
+        <div className={styles.login__container}>
+          <div className={styles.login__header}>
+            <div className={styles.back__svg}>
+              <BiLeftArrowAlt></BiLeftArrowAlt>
+            </div>
+            <span> We'd happy to join us ! <Link href="/">Go Store</Link></span>
+          </div>
+        </div>
+      </div>
+      <Footer country={country}></Footer>
+    </>
   )
+}
+
+export async function getServerSideProps() {
+  return{
+    props:{
+      country: { name: "Indonesia", flag: "./images/indonesia.png",},
+    }
+  }
 }
