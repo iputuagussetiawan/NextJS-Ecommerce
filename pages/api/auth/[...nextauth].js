e1464a13451513b77d6cb9f790dd35b2b40e2220
iputuagussetiawan <agussetiawaniputu@gmail.com>
2023-01-21 16:40:14 +0800

    Callbacks in Next auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -64,6 +64,15 @@ export default NextAuth({
     })
    
   ],
+  callbacks: {
+    async session({ session, token }) {
+      let user = await User.findById(token.sub);
+      session.user.id = token.sub || user._id.toSting();
+      session.user.role = user.role || "user";
+      token.role = user.role || "user";
+      return session;
+    },
+  },
   pages:{
     signIn:"/signin",
   },
