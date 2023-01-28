1e095f75f15001e2a29b11df4c22600ecadda824
iputuagussetiawan <agussetiawaniputu@gmail.com>
2023-01-28 17:22:35 +0800

    Product card 3

--- components/productCard/index.js
+++ components/productCard/index.js
@@ -4,10 +4,10 @@ import { useState } from "react";
 import ProductSwiper from "./ProductSwiper";
 import styles from "./styles.module.scss";
 
-export default function ProductCard({product}) {
- const [active, setActive]=useState(0);
- const [images, setImages] = useState(product.subProducts[active]?.images);
- const [prices, setPrices] = useState(
+export default function ProductCard({ product }) {
+  const [active, setActive] = useState(0);
+  const [images, setImages] = useState(product.subProducts[active]?.images);
+  const [prices, setPrices] = useState(
     product.subProducts[active]?.sizes
       .map((s) => {
         return s.price;
@@ -35,12 +35,56 @@ export default function ProductCard({product}) {
   }, [active, product]);
   return (
     <div className={styles.product}>
-      <Link href={`/product/${product.slug}?style=${active}`}>
-        <div>
-            <ProductSwiper images={images}></ProductSwiper>
+      <div className={styles.product__container}>
+        <a href={`/product/${product.slug}?style=${active}`} target="_blank">
+          <div>
+            <ProductSwiper images={images} />
+          </div>
+        </a>
+        {product.subProducts[active].discount ? (
+          <div className={styles.product__discount}>
+            -{product.subProducts[active].discount}%
+          </div>
+        ) : (
+          ""
+        )}
+        <div className={styles.product__infos}>
+          <h1>
+            {product.name.length > 45
+              ? `${product.name.substring(0, 45)}...`
+              : product.name}
+          </h1>
+          <span>
+            {prices.length === 1
+              ? `USD${prices[0]}$`
+              : `USD${prices[0]}-${prices[prices.length - 1]}$`}
+          </span>
+          <div className={styles.product__colors}>
+            {styless &&
+              styless.map((style, i) =>
+                style.image ? (
+                  <img
+                    src={style.image}
+                    className={i == active && styles.active}
+                    onMouseOver={() => {
+                      setImages(product.subProducts[i].images);
+                      setActive(i);
+                    }}
+                    alt=""
+                  />
+                ) : (
+                  <span
+                    style={{ backgroundColor: `${style.color}` }}
+                    onMouseOver={() => {
+                      setImages(product.subProducts[i].images);
+                      setActive(i);
+                    }}
+                  ></span>
+                )
+              )}
+          </div>
         </div>
-      </Link>
+      </div>
     </div>
-  )
+  );
 }
-
