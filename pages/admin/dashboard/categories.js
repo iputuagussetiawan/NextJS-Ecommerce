8a130a9aef380214e8dd5a32091e8af1b73f128c
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2023-04-07 17:28:03 +0800

    list all categories

--- pages/admin/dashboard/categories.js
+++ pages/admin/dashboard/categories.js
@@ -3,24 +3,25 @@ import db from "../../../utils/db";
 import Category from "../../../models/Category";
 import { useState } from "react";
 import Create from "../../../components/admin/categories/Create";
+import List from "../../../components/admin/categories/List";
 export default function categories({ categories }) {
     const [data, setData] = useState(categories);
-    console.log(data);
+    //console.log(data);
     return (
         <Layout>
-        <div>
-            <Create setCategories={setData}></Create>
-        </div>
+            <div>
+                <Create setCategories={setData}></Create>
+                <List categories={data} setCategories={setData} />
+            </div>
         </Layout>
     );
 }
-
 export async function getServerSideProps(context) {
     db.connectDb();
     const categories = await Category.find({}).sort({ updatedAt: -1 }).lean();
     return {
         props: {
-        categories: JSON.parse(JSON.stringify(categories)),
+            categories: JSON.parse(JSON.stringify(categories)),
         },
     };
 }
