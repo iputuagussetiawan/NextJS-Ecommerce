2e6206be14ceff0cfabd902013b7bf7a1fe900d7
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2023-04-07 20:45:29 +0800

    coupons dates picker

--- components/admin/coupons/Create.js
+++ components/admin/coupons/Create.js
@@ -5,19 +5,35 @@ import * as Yup from "yup";
 import AdminInput from "../../inputs/adminInput";
 import { toast } from "react-toastify";
 import axios from "axios";
+import { LocalizationProvider,DesktopDatePicker  } from '@mui/x-date-pickers';
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import TextField from '@mui/material/TextField';
 export default function Create({ setCoupons }) {
     const [name, setName] = useState("");
     const [discount, setDiscount] = useState(0);
+    const tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(tomorrow);
+
+    const handleStartDate = (newValue) => {
+        setStartDate(newValue);
+    };
+    const handleEndDate = (newValue) => {
+        setEndDate(newValue);
+    };
+
+    console.log(startDate, endDate)
     
     const validate = Yup.object({
         name: Yup.string()
-        .required("Coupon name is required.")
-        .min(2, "Coupon name must be bewteen 2 and 30 characters.")
-        .max(30, "Coupon name must be bewteen 2 and 30 characters."),
+            .required("Coupon name is required.")
+            .min(2, "Coupon name must be bewteen 2 and 30 characters.")
+            .max(30, "Coupon name must be bewteen 2 and 30 characters."),
         discount: Yup.number()
-        .required("Discount is required.")
-        .min(1, "Discount must be atleast 1%")
-        .max(99, "Discount must be 99% or less"),
+            .required("Discount is required.")
+            .min(1, "Discount must be atleast 1%")
+            .max(99, "Discount must be 99% or less"),
     });
     const submitHandler = async () => {
         try {
@@ -49,7 +65,7 @@ export default function Create({ setCoupons }) {
             initialValues={{ name, discount }}
             validationSchema={validate}
             onSubmit={() => {
-            submitHandler();
+                submitHandler();
             }}
         >
             {(formik) => (
@@ -69,6 +85,26 @@ export default function Create({ setCoupons }) {
                     placholder="Discount"
                     onChange={(e) => setDiscount(e.target.value)}
                 />
+                <div className={styles.date_picker}>
+                    <LocalizationProvider dateAdapter={AdapterDateFns}>
+                        <DesktopDatePicker
+                            label="Start Date"
+                            inputFormat="MM/dd/yyyy"
+                            value={startDate}
+                            onChange={handleStartDate}
+                            renderInput={(params) => <TextField {...params} />}
+                            minDate={new Date()}
+                        />
+                        <DesktopDatePicker
+                            label="End Date"
+                            inputFormat="MM/dd/yyyy"
+                            value={endDate}
+                            onChange={handleEndDate}
+                            renderInput={(params) => <TextField {...params} />}
+                            minDate={tomorrow}
+                        />
+                    </LocalizationProvider>
+                </div>
                 <div className={styles.btnWrap}>
                     <button type="submit" className={`${styles.btn} `}>
                         <span>Add Coupon</span>
